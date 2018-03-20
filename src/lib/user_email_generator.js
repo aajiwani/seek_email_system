659929eee173bc90910f152ce0eb552ad5f65b29
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2018-03-20 19:36:55 +0800

    Completes the case study

--- src/lib/user_email_generator.js
+++ src/lib/user_email_generator.js
@@ -1,7 +1,27 @@
 import UserEmail from "@app/src/model/user_email";
+import userStatus from "@app/src/model_helpers/user_status";
 import faker from "faker";
 import moment from "moment";
 import randomDate from "@app/src/lib/random_date";
+
+const dateGenerator = status => {
+  switch (status) {
+    case userStatus.ACTIVE:
+      return moment()
+        .subtract(1, "d")
+        .toDate();
+
+    case userStatus.NOT_RESPONSIVE:
+      return moment()
+        .subtract(3, "d")
+        .toDate();
+
+    case userStatus.INACTIVE:
+      return moment()
+        .subtract(4, "d")
+        .toDate();
+  }
+};
 
 const generator = {
   generate: users => {
@@ -10,12 +30,7 @@ const generator = {
       result.push(
         new UserEmail({
           user: user,
-          last_sent: randomDate(
-            moment()
-              .subtract(7, "d")
-              .toDate(),
-            moment().toDate()
-          ),
+          last_sent: dateGenerator(user.status),
         })
       );
     }
@@ -23,5 +38,12 @@ const generator = {
     return result;
   },
 };
+
+// randomDate(
+//             moment()
+//               .subtract(7, "d")
+//               .toDate(),
+//             moment().toDate()
+//           ),
 
 export default generator;
